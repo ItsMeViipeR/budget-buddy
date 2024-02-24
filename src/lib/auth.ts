9cbf11782b0e26962724dae59469c89a5a94c104
Nicolas AMATI <skkyz3r.dev@gmail.com>
2024-02-24 09:46:05 +0100

    feat: :heavy_plus_sign: Add project's dependencies

---
+++ src/lib/auth.ts
@@ -0,0 +1,40 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import { prisma } from "@/src/lib/prisma";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const { auth, handlers } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      type: "credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const user = await prisma.user.findFirst({
+          where: {
+            email: String(credentials.email),
+            password: String(credentials.password),
+          },
+        });
+
+        if (user) {
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXT_AUTH_SECRET,
+});
